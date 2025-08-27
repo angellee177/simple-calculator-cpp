4a2caf404bddaa42acd2a995095a7df7459c1c19
angellee177 <angelriapurnamasari17@gmail.com>
2025-08-27 17:06:23 +0700

    chore: add more calculator function

--- include/Calculator.h
+++ include/Calculator.h
@@ -1,10 +1,26 @@
 #ifndef CALCULATOR_H // check if the header already define
 #define CALCULATOR_H // define the Calculator header
+#include <string>
+
+using namespace std;
 
 class Calculator {
 public:
-    // function to perform calculation
-    double calculate(double num1, char op, double num2);
+    // Takes a string expression, evaluates it,
+    // and returns the result as a string.
+    string evaluateExpression(const string& expression);
+
+    // Clear the current expression and the last result
+    void clear();
+
+    // Clears only the last character of the current expression
+    void backspace();
+
+    // Return the current state of the expression as a string.
+    string getExpression();
+
+    // return the previous result as a string for history display.
+    std::string getHistoryResult();
 };
 
 #endif
